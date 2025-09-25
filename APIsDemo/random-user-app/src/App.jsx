4f4fd469ad89d8de5f6a74ab9a041a453308c109
Rohit Bansode <rohitbansode.tech@gmail.com>
2025-09-25 11:49:03 +0530

    Add One Button For Getting New User

--- APIsDemo/random-user-app/src/App.jsx
+++ APIsDemo/random-user-app/src/App.jsx
@@ -4,19 +4,28 @@ import './App.css'
 function App() {
   const[user, setUser]=useState(null);
   const[loading, setLoading]= useState(true);
-   useEffect(() => {
+
+   const fetchUser= () => {
+     setLoading(true);
     fetch("https://randomuser.me/api/")
       .then((res) => res.json())
       .then((data) => {
           setUser(data.results[0]);
           setLoading(false);
         })
-      .catch((err) => console.error('Error:', err));
+      .catch((err) => {
+        console.error('Error:', err);
+         setLoading(false);
+      });
+    };
+   useEffect(() =>{
+    fetchUser();
+   }, []);
 
-   },[]);
    if(loading){
     return <h2 className="text-center mt-10">Loading....</h2>;
    }
+
   return (
     <>
      <div className="flex justify-center item-center h-screen bg-gray-100">
@@ -25,6 +34,7 @@ function App() {
         <h1 className="text-xl font-bold">{user.name.first} {user.name.last}</h1>
         <p className="text-gray-600">{user.email}</p>
         <p className="text-gray-500 mt-2 "> {user.location.city}, {user.location.country}</p>
+        <button className="mt-6 px-4 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition" onClick={fetchUser}> GET NEW USER</button>
       </div>
      </div>
       
